198b420cc146f074eeebb9e867a83f5598f0e955
Andrey Utkin <spaun@vda-team.com>
2015-04-02 11:34:15 +0600

    Fix the upsert query for non-integer primary keys

--- src/Relational/Driver/Mysqli/MysqlQueryBuilder.php
+++ src/Relational/Driver/Mysqli/MysqlQueryBuilder.php
@@ -5,6 +5,7 @@ use Vda\Datasource\Relational\QueryBuilder;
 use Vda\Datasource\Relational\QueryBuilderState;
 use Vda\Query\Upsert;
 use Vda\Query\Operator\Operator;
+use Vda\Util\Type;
 
 class MysqlQueryBuilder extends QueryBuilder
 {
@@ -38,7 +39,7 @@ class MysqlQueryBuilder extends QueryBuilder
 
         if (!empty($t->_primaryKey) && count($keyFields) == 1) {
             $pk = $t->getField(reset($keyFields));
-            if (!in_array($pk, $updateFields)) {
+            if ($pk->getType() == Type::INTEGER && !in_array($pk, $updateFields)) {
                 $updateFields[] = $pk;
                 $updateValues[] = Operator::call('LAST_INSERT_ID', array($pk));
             }
